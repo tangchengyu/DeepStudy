f4f6b2721993b9886e50bab2740517d53e5b3b05
tangchengyu <181341732+tangchengyu@users.noreply.github.com>
2026-09-22 19:15:48 +0800

    Release version 1.2.53

--- test/packaging-icons.test.js
+++ test/packaging-icons.test.js
@@ -16,14 +16,14 @@ function pngDimensions(filePath) {
 }
 
 test("master desktop package uses the next public master version", () => {
-  assert.equal(pkg.version, "1.2.52");
+  assert.equal(pkg.version, "1.2.53");
   assert.doesNotMatch(pkg.version, /local/i);
 });
 
 test("Android pilot package uses the same public version as the desktop app", () => {
   assert.equal(mobilePkg.version, pkg.version);
   assert.match(androidBuildGradle, new RegExp(`versionName "${pkg.version}"`));
-  assert.match(androidBuildGradle, /versionCode 1252/);
+  assert.match(androidBuildGradle, /versionCode 1253/);
 });
 
 test("packaging uses the DeepStudy clock icon on macOS and Windows", () => {
